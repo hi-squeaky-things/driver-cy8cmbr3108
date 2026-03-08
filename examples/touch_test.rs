4ab0fd7aa1ec106c33384d3bfc19595399a27c07
Daniël <daniel@hi-squeaky-things.nl>
2026-03-08 19:45:40 +0100

    update stuff

--- examples/touch_test.rs
+++ examples/touch_test.rs
@@ -1,28 +1,24 @@
 #![no_std]
 #![no_main]
 
-use driver_cy8cmbR3108::{
-    registers, CY8CMBR3108, CY8CMBR3XXX_CTRL_CMD_CALC_CRC, CY8CMBR3XXX_CTRL_CMD_RESET,
-};
+use driver_cy8cmbR3108::CY8CMBR3108;
 use esp_backtrace as _;
 use esp_hal::{
     delay::Delay,
     i2c::master::{Config, I2c},
-    time::Rate,
     xtensa_lx_rt::entry,
 };
 use esp_println::println;
-
 esp_bootloader_esp_idf::esp_app_desc!();
 
 #[entry]
 fn main() -> ! {
     // init CPU
-    let mut config = esp_hal::Config::default().with_cpu_clock(esp_hal::clock::CpuClock::_240MHz);
+    let config = esp_hal::Config::default().with_cpu_clock(esp_hal::clock::CpuClock::_240MHz);
     let mut peripherals = esp_hal::init(config);
 
-    let mut config = Config::default();
-    let mut i2c = I2c::new(peripherals.I2C0.reborrow(), config)
+    let config = Config::default();
+    let i2c = I2c::new(peripherals.I2C0.reborrow(), config)
         .unwrap()
         .with_sda(peripherals.GPIO47)
         .with_scl(peripherals.GPIO48);
@@ -30,20 +26,8 @@ fn main() -> ! {
     let delay = Delay::new();
 
     let mut touch = CY8CMBR3108::new(i2c, delay);
+    let _ = touch.wake_up();
 
-    for i in 0..5 {
-        let is_device_ready = touch.ready();
-        match is_device_ready {
-            Ok(true) => {
-                println!("CY8CMBR3108 Initialization successful!");
-                break;
-            }
-            Ok(false) => {
-                println!("CY8CMBR3108 Initialization failed: Device not ready after 3 attempts.")
-            }
-            Err(e) => println!("CY8CMBR3108 Initialization failed with error: {:?}", e),
-        }
-    }
     //device id = 2563
     println!(
         "CY8CMBR3108 Device Identifier : {:?} (=2563)",
@@ -58,12 +42,12 @@ fn main() -> ! {
     let is_config_ready = touch.write_configuration();
     match is_config_ready {
         Ok(true) => {
-            println!("CY8CMBR3108 Config successful!");
+            println!("CY8CMBR3108 Configuration successful!");
         }
         Ok(false) => {
-            println!("CY8CMBR3108 Config failed,")
+            println!("CY8CMBR3108 Configuration failed.")
         }
-        Err(e) => println!("CY8CMBR3108 Config failed with error: {:?}", e),
+        Err(e) => println!("CY8CMBR3108 Configuration failed with error: {:?}", e),
     }
 
     loop {
